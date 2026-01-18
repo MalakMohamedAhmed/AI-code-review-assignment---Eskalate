ecd8dc7feadf6d0323c1bf747210adc1c9a71dc7
MalakMohamedAhmed <168978331+MalakMohamedAhmed@users.noreply.github.com>
2026-01-18 07:25:51 +0200

    Add files via upload

---
+++ correct_task3.py
@@ -0,0 +1,45 @@
+import math
+
+# added this function for making sure that the "amount" is a numerical value, not a null, invaled, or any other thing
+def is_numeric_string(value):
+    try:
+        float(value) # try converting it to a float, if it was converted successfully then it has a valied value
+        if math.isnan(value) or math.isinf(value): # if the value is infinity or nan it should be ignored
+            return False
+        return True
+    except (ValueError, TypeError): # otherwise we should throw an error
+        return False
+
+def average_valid_measurements(values): # assumed that "values" is always going to be a list
+    total_of_values = 0
+    number_of_not_null_values = 0 # for counting the actual not null values
+    total_number_of_values = len(values) # number of all the values
+
+    if total_number_of_values == 0: # in case 'values' was completly empty
+        print("There exists no values")
+        return 0
+
+    for value in values:
+        if value is not None:
+            if is_numeric_string(value): # in case the value weren't a valied thing
+                number_of_not_null_values += 1
+                total_of_values += float(value)
+
+    if number_of_not_null_values == 0: # in case all the values were null
+        print("All the values are null !!")
+        return 0
+    
+    return round(total_of_values / number_of_not_null_values, 10) # in case we added many floates, to fix precision issues
+
+
+# Some Test cases i used to make sure i handled all the edge cases i mentioned
+
+# values = [10, None, 20, 30]
+# values = []
+# values = [None, None, None]
+# values = [10, "abc", 20]
+# values = [10, [1, 2], 20]
+# values = [10, float('inf'), 20]
+# values = [10, float('nan'), 20]
+# values = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+# print(average_valid_measurements(values))
